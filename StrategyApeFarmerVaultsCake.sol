ad656b6a89dbf9478dad8d1d9656395d33872649
kingdonutdev3@gmail.com <kingdonutdev3@gmail.com>
2021-08-14 13:16:31 -0400

    Fix for incorrect wantAmtWithFee calculation

--- StrategyApeFarmerVaultsCake.sol
+++ StrategyApeFarmerVaultsCake.sol
@@ -1333,7 +1333,7 @@ contract StrategyApeFarmerVaultsCake is Ownable, ReentrancyGuard, Pausable {
 
         uint256 wantAmtWithFee = _wantAmt;
         if (withdrawFeeFactor < withdrawFeeFactorMax) {
-            wantAmtWithFee = _wantAmt.mul(withdrawFeeFactor).div(withdrawFeeFactorMax);
+            wantAmtWithFee = _wantAmt.add(_wantAmt.mul(withdrawFeeFactor).div(withdrawFeeFactorMax));
         }
 
         uint256 sharesRemoved = wantAmtWithFee.mul(sharesTotal).div(wantLockedTotal);
